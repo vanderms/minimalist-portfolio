f4c82042896a69dfe8c40fe3f0d616481be2584d
vanderms <vanderms.84@gmail.com>
2022-07-27 22:38:51 -0300

    feat: create sidebar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,37 @@
+import { Link, NavLink } from "react-router-dom";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { useState } from "react";
+
 export function Navbar() {
-  return <nav>Navbar</nav>
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  return (
+    <nav>
+      <div className="container relative pt-8 flex justify-between items-center">
+        <Link to="/" aria-label="Homepage">
+          <img src="/assets/logo.svg" alt="" />
+        </Link>
+        <button aria-label="toggle menu" onClick={() => setMenuOpen((prev) => !prev)}>
+          {menuOpen ? (
+            <IoClose className="w-6 h-6 text-neutral-800" />
+          ) : (
+            <IoMenu className="w-6 h-6 text-neutral-800" />
+          )}
+        </button>
+        <div className="absolute top-[5.5rem] right-0 w-[13.9375rem] py-10 bg-neutral-800">
+          <ul className="flex flex-col items-center gap-8">
+            <li>
+              <NavLink to="/">HOME</NavLink>
+            </li>
+            <li>
+              <NavLink to="/portfolio">PORTFOLIO</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">CONTACT ME</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,22 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "primary": "var(--primary)",
+      "secondary": "var(--secondary)",
+      "danger": "var(--danger)",
+      "neutral-50": "var(--neutral-50)",
+      "neutral-100": "var(--neutral-100)",
+      "neutral-200": "var(--neutral-200)",
+      "neutral-500": "var(--neutral-500)",
+      "neutral-800": "var(--neutral-800)",
+      "transparent": "transparent",
+    },
+
     screens: {
       sm: '19.4375rem',
       md: '45rem',
       xl: '74rem',
-      xxl: '90.25rem'
     },
     fontFamily: {
       sans: "'Public Sans', sans-serif",
